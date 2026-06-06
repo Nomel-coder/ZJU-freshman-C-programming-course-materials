683d6e7e403f05e870d60d08b6930c17cee4ba81
Nomel-Coder <1877678267@qq.com>
2026-06-06 11:31:10 +0800

    首次提交代码

---
+++ out/build/自定义配置预设/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/c/Users/Lemon/Desktop/C语言程序设计
+# Build directory: /mnt/c/Users/Lemon/Desktop/C语言程序设计/out/build/自定义配置预设
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
